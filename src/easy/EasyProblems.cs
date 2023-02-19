ff792ed4fe0e7789e7a5431e40b894d87f53bd36
mamma-girl <future.mamaa.girl@gmail.com>
2023-02-19 22:18:13 +1100

    Initial commit and first problem with tests

---
+++ src/easy/EasyProblems.cs
@@ -0,0 +1,21 @@
+﻿namespace easy
+{
+	public class EasyProblems
+	{
+		// https://leetcode.com/problems/two-sum/
+		public int[] TwoSum_1(int[] nums, int target)
+		{
+			for (var i = 0; i < nums.Length - 1; i++)
+			{
+				for (var j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[i] + nums[j] == target)
+					{
+						return new int[] { i, j };
+					}
+				}
+			}
+			return new int[] { 0, 0 };
+		}
+	}
+}
---
+++ src/easyTests/EasyProblemsTests.cs
@@ -0,0 +1,26 @@
+﻿using easy;
+using NUnit;
+using NUnit.Framework;
+
+namespace easyTests
+{
+	[TestFixture]
+	public class EasyProblemsTests
+	{
+		private EasyProblems _easyProblems = new();
+		[OneTimeSetUp]
+		public void Setup()
+		{
+
+		}
+		[TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+		[TestCase(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+		[TestCase(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+		[TestCase(new int[] { 2, 5, 5, 11 }, 10, new int[] { 1, 2 })]
+
+		public void TwoSum_1Test(int[] nums, int target, int[] output)
+		{
+			Assert.That(_easyProblems.TwoSum_1(nums, target), Is.EquivalentTo(output));
+		}
+	}
+}
